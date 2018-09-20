661c24bf0ca81b987364ac7e233d8003c0570aa3
kammoji <juhana@konekettu.fi>
2018-09-20 10:47:29 +0300

    Inserted test data stream...

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int stream(int data);
+
+
+
+int stream(int data){
+
+	printf("%d", data);
+	printf("\n");
+	return EXIT_SUCCESS;
+
+}
 
 int main(void){
 
@@ -7,6 +22,15 @@ printf("\nThis is aiplane\n-----------\n\n");
 
 printf("Please start data stream...\n");
 
+printf("Testing integer data stream:\n");
+for(int i=0; i<8; i++){
+
+	stream(8);
+}
+
+cout << "\nDone.";
+printf("\n\n");
 return EXIT_SUCCESS;
 
 }
+
